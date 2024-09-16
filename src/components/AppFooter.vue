19bd2aab6f316b9543002f47657be736e4eb9b71
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-16 18:57:12 +0200

    added generic structure for main and footer. Added colors, text, alignments and images. Now its time to make the data dynamic

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,13 +9,145 @@ export default {
 
 <template>
     <footer>
-        <div class="container">
-            <h1>
-            footer
-            </h1>        
+        <div class="footer-top">
+            <div class="footer-links">
+                <section id="comics">
+                    <ul>
+                        <li>
+                            <h4>DC COMICS</h4>
+                            <a href="#">CHARACTERS</a>
+                            <a href="#">COMICS</a>
+                            <a href="#">MOVIES</a>
+                            <a href="#">TV</a>
+                            <a href="#">GAMES</a>
+                            <a href="#">VIDEO</a>
+                            <a href="#">NEWS</a>
+                        </li>
+                        <li>
+                            <h4>SHOP</h4>
+                            <a href="#">SHOP DC</a>
+                            <a href="#">SHOP DC COLLECTIBLES</a>
+                        </li>
+                    </ul>
+                </section>
+                <section id="dc">
+                    <ul>
+                        <li>
+                            <h4>DC</h4>
+                            <a href="#">Terms of use</a>
+                            <a href="#">Privacy Policy(new)</a>
+                            <a href="#">Ad choices</a>
+                            <a href="#">Advertising</a>
+                            <a href="#">Jobs</a>
+                            <a href="#">Subscritions</a>
+                            <a href="#">Talent Workshop</a>
+                            <a href="#">CSPC Certificates</a>
+                            <a href="#">Ratings</a>
+                            <a href="#">Shop Help</a>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </section>
+                <section id="sites">
+                    <ul>
+                        <li>
+                            <h4>SITES</h4>
+                            <a href="#"> DC</a>
+                            <a href="#">MAD Magazine</a>
+                            <a href="#">DC Kids</a>
+                            <a href="#">DC Universe</a>
+                            <a href="#">DC Power visa</a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+            <div class="logo-footer">
+                <img src="../assets/img/dc-logo-bg.png" alt="logo">
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="sign-up">
+                <button> SIGN-UP NOW!</button>
+            </div>
+            <div class="socials">
+                <span class="follow">FOLLOW US</span>
+                <img src="../assets/img/footer-facebook.png" alt="facebook">
+                <img src="../assets/img/footer-twitter.png" alt="twitter">
+                <img src="../assets/img/footer-youtube.png" alt="youtube">
+                <img src="../assets/img/footer-pinterest.png" alt="pinterest">
+                <img src="../assets/img/footer-periscope.png" alt="periscope">
+            </div>
         </div>
     </footer>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+footer{
+    color: grey;
+    h4{
+        color: white;
+    }
+    .footer-top{
+        height: 350px;
+        background-image: url(../assets/img/footer-bg.jpg);
+        display: flex;
+        align-items: center;
+        .footer-links{
+            display: flex;
+            flex-basis: 60%;
+            align-items: baseline;            
+            ul{
+                margin: 20px 0;
+                
+                li{
+                    display: flex;
+                    flex-direction: column;
+                    align-items: baseline;
+                    gap: 5px;
+                    padding: 0 20px;
+
+                    a{
+                        color: grey;
+                    }
+                }
+            }
+        }
+        .logo-footer{
+            display: flex;
+            flex-basis: 40%;
+            justify-content: center;
+            img{
+                height: 300px;
+            }
+        }
+    }
+    .footer-bottom{
+        height: 100px;
+        background-color: #303030;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding : 0 20px;
+
+        .sign-up{
+            button{
+                color: white;
+                padding: 10px 15px;
+                border-color: blue;
+                background-color: #303030;
+                cursor: pointer;
+            }
+        }
+        .socials{
+            display: flex;
+            align-items: center;
+            gap: 15px;
+            cursor: pointer;
+            .follow{
+                color: blue;
+            }
+        }
+    }
+
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,13 +9,75 @@ export default {
 
 <template>
     <main>
-        <div class="container">
-            <h1>
-            main
-            </h1>
-        </div>
+            <section class="jumbotron">
+                <div class="top-jumbo">
+                     --Content goes here--
+                </div>
+                 <div class="bottom-jumbo">
+                    <ul>
+                        <li>
+                            <img src="../assets/img/buy-comics-digital-comics.png" alt="comics">
+                            <span> DIGITAL COMICS</span>
+                        </li>
+                        <li>
+                            <img src="../assets/img/buy-comics-merchandise.png" alt="merch">
+                            <span>DC MERCHANDISE</span>
+                        </li>
+                        <li>
+                            <img src="../assets/img/buy-comics-subscriptions.png" alt="subscription">
+                            <span>SUBSCRIPTION</span>
+                        </li>
+                        <li>
+                            <img src="../assets/img/buy-comics-shop-locator.png" alt="locator">
+                            <span>COMIC SHOP LOCATOR</span>
+                        </li>
+                        <li>
+                            <img class="img-special" src="../assets/img/buy-dc-power-visa.svg" alt="visa">
+                            <span>DC POWER VISA</span>
+                        </li>
+                    </ul>
+                        
+                </div>
+            </section>
     </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+    main{
+        .jumbotron{
+            color: white;
+            .top-jumbo{
+                height: 200px;
+                background-color: black;
+                font-size: 30px;
+                display: flex;
+                align-items: center;
+                padding-left: 15px;
+            }
+            .bottom-jumbo{
+                height: 300px;
+                background-color: blue;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                ul{
+                    display: flex;
+                    align-items: center;
+                    li{
+                        display: flex;
+                        align-items: center;
+                        padding: 0 30px;
+                        gap: 5px;
+
+                        .img-special{
+                           width:80px;
+                           height: 90px;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
 </style>
